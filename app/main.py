527fa8dd02d83dcc378ae3d2b1953e12ebaf475c
ghghyo <67969887+ghghyo@users.noreply.github.com>
2021-10-08 11:31:42 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,23 +6,29 @@ Created on Thu Oct  7 13:45:40 2021
 @author: yabubaker
 """
 
+
 from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS, cross_origin
 
 from app.torch_utils import accept_input, get_prediction
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 # A welcome message to test our server
 @app.route('/')
+@cross_origin()
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
-
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     if request.method == 'POST':
         #print(request.data)
         #file = request.form.get('number')
         file=request.form['nm']
+ 
         img=accept_input(int(file))
          
         boxes = get_prediction(img)
